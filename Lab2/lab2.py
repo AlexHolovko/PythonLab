f24e2a7bde8ec98d317dfce510a9a21be96e63cc
AleksandrHolovko <golovko.sanya2@gmail.com>
2024-12-04 21:51:35 +0200

    Lab2

---
+++ Lab2/lab2.py
@@ -0,0 +1,23 @@
+# task 1
+print("Programming***Essentials***in...", end="")
+print("Python\n")
+# task 2
+print("    *")
+print("   * *")
+print("  *   *")
+print(" *     *")
+print("***   ***")
+print("  *   *")
+print("  *   *")
+print("  *****\n")
+# task 3
+print("I'm student\n")
+# task 4
+print("\"I\'m\"\n\"learning\"\n\"\"\"Python\"\"\"\n")
+# task 5
+decimal_500 = int("500", 8)
+print(decimal_500)
+# task 6
+decimal_777 = int("777", 16)
+print(decimal_777)
+
